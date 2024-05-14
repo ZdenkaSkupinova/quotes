4cea810b096cb14107a54b309021bc93f672d002
devleinka <42700247+devleinka@users.noreply.github.com>
2024-05-14 22:04:29 +0200

    preidané komentáre s nápovedou na opravy

--- quotes/src/pages/index.jsx
+++ quotes/src/pages/index.jsx
@@ -4,7 +4,16 @@ import './index.css';
 import { randomQuote } from "./quotes.js"
 import { renderQuotes } from './quotes.js';
 import { moveToTop } from './quotes.js';
+// keď zavoláš takto tie 3 funkcie na ten istý element(#quote),
+// tak bude zobrazená len tá posledná,
+// čo je renderQuotes(), pretože moveToTop() spadne
 
+// prosím uprav index.html a priprav si tam dva rôzne div s rôznymi id, 
+// nad jedným budeš volať randomQuote(), nad druhým renderQuotes().
+// môžeš ich vizuálne oddeliť, aby potom nevyzeralo, že to nefunguje,
+// pretože ten random na sebe nebude mať event listener
+
+// moveToTop sa bude volať event listenerom, tu nie.
 document.querySelector("#quote").textContent = randomQuote()
 document.querySelector("#quote").textContent = renderQuotes()
 document.querySelector("#quote").textContent = moveToTop()
--- quotes/src/pages/quotes.js
+++ quotes/src/pages/quotes.js
@@ -10,21 +10,44 @@ const quotes = [
     "It is never too late to be what you might have been",
     "A day without sunshine is like, you know, night"
     ]
+
+    // ak chceš random index použiť viackrát, urob si na neho funkcu 
+    // a tú volaj na rôznych miestach
+
+    //napr. const randomIndex = ()=> {
+    //  return Math.floor(Math.random() * quotes.length)  
+    //}
+    // 
     
     export const randomQuote = () => {
         const randomIndex = Math.floor(Math.random() * quotes.length)
-    
+    // keď budeš mať funkciu random index, tak ju môžeš použiť takto:
+    // return quotes[randomIndex()]
     return quotes[randomIndex]
     }
 
     export const moveToTop = (index) => {
       // jak tam dostanu index už náhodně vybraného citátu z randomQuote???
+      // zavoláš si randomIndex() a uložíš výsledok do premennej, a tú použiješ
+      // ale muyslím, že presun na začiatok sa nevzťahuje na úlohu zobraziť náhodný quote, ale na pole, takže to nerob :)
+
+      //keď ho iba zmažeme, tak oňho prídeme.
+      // musíme uložiť do premennej, takto:
+      // const quote = quotes.splice(index, 1)[0]
         quotes.splice(index, 1) // smaže ho na řádku, kde je
+      // na začiatok poľa pridáva položky funkcia unshift()
+      // dokumentácia tu: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+      // takže nad quotes zavoláš unshift a dáš jej do zátvoriek quote
         quotes.splice(index, 0, randomQuote) // přidá na začátek, 1.řádek = 0 index 
+        // takto splice nefunguje
     }
     
 
     export const renderQuotes = () => {
+      // toto nefunguje, lebo chceš vybrať eden element, ale voláš querySelectorAll, ktorý vyberá všetky a vracia pole elementov,
+      // preto to spadne, lebo na pole nemôžeš zavolať .innerHTML
+      // použi querySelector()
+      // a nezabudni na zmenu v index.html popísanú v index.jsx
       const quote = document.querySelectorAll('#quote');
       quote.innerHTML = '';
       quotes.forEach((quote) => {
@@ -35,7 +58,15 @@ const quotes = [
         `;  
       })
       quotes = document.querySelectorAll('.quote');
+      // querySelectorAll() vracia pole, 
+      // eventListener nemôžeš zavesiť na pole, iba na jednotlivé elementy,
+      // musíš to pole quotes prejsť forEach() a každému z nich dať eventListener
+
+      // a move to top potrebuje index, takže forEach volaj ako
+      // ... .forEach((item, index) => ....)
+      // a moveTo Top dostane index ako argument
       quotes.addEventListener('click', moveToTop)
+      // Čo je renderList???
       renderList();
     };
       
